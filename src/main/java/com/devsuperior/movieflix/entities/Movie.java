683238339150efaea516733cfe25101c45975f33
Alexsandro Machado <lekomachado@hotmail.com>
2022-05-26 15:40:44 -0300

    Security infrainstructure, database seed, resource, service and repository

--- src/main/java/com/devsuperior/movieflix/entities/Movie.java
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -16,6 +19,8 @@ import javax.persistence.Table;
 public class Movie implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String subTitle;
